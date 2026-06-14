9bba53ccd36074cb1ae73bacb4d767cd35788744
Aryan Shirke <aaryana1u5s.shirke@gmail.com>
2026-06-14 19:04:01 +0530

    Add JavaScript for navbar and video controls

---
+++ script.js
@@ -0,0 +1,61 @@
+      const navbar = document.getElementById("navbar");
+      const body = document.body;
+      window.addEventListener("scroll", () => {
+        navbar.classList.toggle("scrolled", window.scrollY > 50);
+      });
+      window.addEventListener("scroll", () => {
+        navbar.classList.toggle("scrolled", window.scrollY > 50);
+      });
+    </script>
+
+    <!-- background video sound mute/unmute code -->
+    <script>
+      const heroVideo = document.getElementById("heroVideo");
+
+      document.getElementById("video-section").addEventListener("click", () => {
+        if (heroVideo.muted) {
+          heroVideo.muted = false;
+        } else {
+          heroVideo.muted = true;
+        }
+      });
+    </script>
+
+    <!-- engine button (sound on and off code) -->
+    <script>
+      const engineBtn = document.getElementById("engineBtn");
+      const engineSound = document.getElementById("engineSound");
+
+      let isPlaying = false;
+
+      engineBtn.addEventListener("click", () => {
+        if (!isPlaying) {
+          engineSound.currentTime = 0; // rewind to start
+          engineSound.play();
+          isPlaying = true;
+        } else {
+          engineSound.pause();
+          engineSound.currentTime = 0; // stop and reset
+          isPlaying = false;
+        }
+      });
+    </script>
+
+    <!-- watermark apper code -->
+    <script>
+      const watermark = document.querySelector(".watermark-text");
+
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              watermark.classList.add("visible");
+              observer.unobserve(watermark); // Only animate once
+            }
+          });
+        },
+        {
+          threshold: 0.5, // Trigger when 50% of the section is visible
+        }
+      );
+      observer.observe(watermark);
